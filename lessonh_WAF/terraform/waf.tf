f91caafa8ac9b916624ffdef764469b4281ebfbb
BalericaAI <balericaai@gmail.com>
2026-07-07 15:13:38 -0500

    Add WAF logging configuration resource

--- lessonh_WAF/terraform/waf.tf
+++ lessonh_WAF/terraform/waf.tf
@@ -63,3 +63,19 @@ resource "aws_wafv2_web_acl" "api_acl" {
   }
 
 }
+
+##############################################################
+# WAF Logging
+##############################################################
+
+resource "aws_wafv2_web_acl_logging_configuration" "logging" {
+
+  log_destination_configs = [
+
+    aws_cloudwatch_log_group.waf_logs.arn
+
+  ]
+
+  resource_arn = aws_wafv2_web_acl.api_acl.arn
+
+}
